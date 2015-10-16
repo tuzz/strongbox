23038039eb0b71dac7dc7f82d7c49f402233e76a
Chris Patuzzo <chris@patuzzo.co.uk>
2015-10-17 00:55:23 +0100

    Specify how POST /job/some-id should work

--- spec/strongbox_spec.rb
+++ spec/strongbox_spec.rb
@@ -4,8 +4,23 @@ RSpec.describe Strongbox do
   include Rack::Test::Methods
   let(:app) { subject }
 
-  it "has some specs" do
-    get "/"
-    expect(last_response).to be_ok
+  describe "POST /job/:id" do
+    it "creates a pangram job to be computed" do
+      post "/job/some-id", {
+        prefix: "This sentence has",
+        join: "and"
+      }.to_json
+      expect(last_response).to be_ok
+
+      get "/job/some-id"
+      expect(last_response).to be_ok
+
+      body = JSON.parse(last_response.body)
+      expect(body).to eq(
+        "prefix" => "This sentence has",
+        "join" => "and",
+        "status" => "pending"
+      )
+    end
   end
 end
